361ea1286e1ba178a4507357b30ef37ff48ef87b
anike <aniketsuryawanshixz1@gmail.com>
2022-01-05 18:23:35 +0530

    Configrated template in settings, Setup views Creating Login Model and Map in Db collection

--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def TestFun(request):
+    return render(request, 'LoginTemp.html')
--- SkromanSupport/settings.py
+++ SkromanSupport/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'SkromanSupport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SkromanSupport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'skroman_erp',
+        'HOST': 'skora-db.ckwveljlsuux.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'skora1234'
     }
 }
 
